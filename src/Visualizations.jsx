bef77f5a7328b11853b39662fe28caede7ac4ad0
Colinchang123 <S10242330@connect.np.edu.sg>
2024-07-31 21:48:27 +0800

    Initial commit with project setup

--- src/Visualizations.jsx
+++ src/Visualizations.jsx
@@ -30,10 +30,14 @@ const Generic = () => (
         <div id="main">
           <div className="inner">
             <h1>Visuals of patient data</h1>
-            <span className="image main"><img src={require('./images/vis1.jpg')} alt="" style={{ width: '1000px', height: 'auto' }} /></span>
+            <span className="image main"><img src={require('./images/Viz1.png')} alt="" style={{ width: '1000px', height: 'auto' }} /></span>
+            <li>Patient Demographic : This dashboard provides a comprehensive overview of the patient population demographics and characteristics. It includes visualizations for the distribution of patients by gender, age category, race, priority category, surgical risk category, ASA category, anesthesia type, and smoking status. General key metrics such as the total number of patients, number of ICU patients, number of deaths, and the average age of patients are highlighted at the top. This dashboard aims to provide an understanding the overall makeup of the patient population and identifying any notable trends or distributions.</li>
             <p></p>
-
-            <span className="image main"><img src={require('./images/vis2.jpg')} alt="" style={{ width: '1000px', height: 'auto' }} /></span>
+            <span className="image main"><img src={require('./images/Viz2.png')} alt="" style={{ width: '1000px', height: 'auto' }} /></span>
+            <li>Preoperative Risk Analysis : This dashboard provides an analysis of preoperative risk factors to identify patients at higher risk for adverse outcomes. It includes visualizations for the distribution of patients by ASA category, RCRI score, anemia category, grade of kidney disease, and FTND scores. Key metrics such as the total number of patients are highlighted at the top. This dashboard helps in assessing the preoperative health status of patients, identifying high-risk groups, and understanding the distribution of various risk factors within the patient population. It provides valuable insights for preoperative planning and risk stratification.</li>
+            <p></p>
+            <span className="image main"><img src={require('./images/Viz3.png')} alt="" style={{ width: '1000px', height: 'auto' }} /></span>
+            <li>Postoperative Mortality Analysis : This dashboard focuses on analyzing postoperative mortality data to identify patterns and factors associated with mortality. It includes visualizations for the gender and age distribution of patients who experienced postoperative mortality, their smoking status, FTND scores, ASA category, surgical risk category, anemia category, and 24-hour ICU admission status. Metrics such as the total number of patients, ICU patients, and the average age of patients who experienced mortality are highlighted at the top. This dashboard aids in understanding the demographics and clinical characteristics of patients who died postoperatively, providing insights into potential risk factors contributing to postoperative mortality.</li>
 
           </div>
         </div>
